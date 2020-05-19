d509af645b43ac165fd1421664f34be2182aca6f
Aaron32365 <56372224+Aaron32365@users.noreply.github.com>
2020-05-19 12:30:24 -0700

    added heroku functionality

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-
+//M6xVzXFFMn8epb_
 const PORT = 3000;
 
 const app = express();
-
+//express connections
 app.use(logger("dev"));
 
 app.use(compression());
@@ -15,7 +15,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+//mongoose db connection
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget"
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
